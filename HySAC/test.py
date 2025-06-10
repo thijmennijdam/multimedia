f0faacafaa890997bfa1bdc40bca532f248f2af6
KT-47 <brouwer427@outlook.com>
2025-06-10 10:16:11 +0200

    Can infer textual data

--- HySAC/test.py
+++ HySAC/test.py
@@ -1,4 +1,5 @@
 from hysac.models import HySAC
+from transformers import CLIPTokenizer
 
 print("Loading model...")
 
@@ -7,25 +8,15 @@ model_id = "aimagelab/hysac"
 model = HySAC.from_pretrained(model_id, device="cpu")
 print("Model loaded succesfully!")
 
+tokenizer = CLIPTokenizer.from_pretrained('openai/clip-vit-large-patch14')
 
+# TODO: Use with desired textual data
+input_text = "Hello, world!"
 
+tokenized_text = tokenizer(input_text, return_tensors="pt", padding='max_length', truncation=True)
+print("Tokenized text:", tokenized_text['input_ids'])
 
-# import torch
-# from collections import OrderedDict
+embed_text = model.encode_text(tokenized_text['input_ids'], project=True)
 
-# # Load the original checkpoint
-# state_dict = torch.load("/home/kt-47/.cache/huggingface/hub/models--aimagelab--hysac/snapshots/5166e4d6bd1a311dfe4131c4cc5768cc2f3d809e/hysac_model.pth", map_location="cpu")
+print("Embedded text shape:", embed_text.shape)
 
-# # Create a new state dict with 'base_model' removed from keys
-# new_state_dict = OrderedDict()
-
-# for k, v in state_dict.items():
-#     if "base_model" in k:
-#         new_key = k.replace("base_model.", "")
-#     else:
-#         new_key = k
-#     new_state_dict[new_key] = v
-
-# # Now load into your model
-# # model = HySAC()  # or HySAC.from_config(...) if needed
-# model.load_state_dict(new_state_dict, strict=False)  # or True if keys now match exactly
