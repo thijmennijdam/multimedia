541a6c4b87e7741699b935385ef386f9f958e628
thijmennijdam <thijmen.nijdam@gmail.com>
2025-06-11 12:18:01 +0200

    kinda working code

---
+++ commands.sh
@@ -0,0 +1,26 @@
+
+# SMALLEST POSSIBLE ALLOCATION
+
+# a100
+srun --partition=gpu_a100 --ntasks=1 --gpus=1 --cpus-per-task=18 --time=01:00:00 --pty bash -i
+
+# h100
+srun --partition=gpu_h100 --ntasks=1 --gpus=1 --cpus-per-task=16 --time=01:00:00 --pty bash -i
+
+# FULL ALLOCATION
+
+# genoa
+srun --partition=genoa --ntasks=1 --cpus-per-task=192 --time=01:00:00 --pty bash -i
+
+# a100
+srun --partition=gpu_a100 --ntasks=1 --gpus=4 --cpus-per-task=72 --time=01:00:00 --pty bash -i
+
+# h100
+srun --partition=gpu_h100 --ntasks=1 --gpus=4 --cpus-per-task=64 --time=01:00:00 --pty bash -i
+
+module purge
+module load 2024
+module load Anaconda3/2024.06-1
+
+
+ssh -L 5901:tcn679:5901 scur1160@snellius.surf.nl
